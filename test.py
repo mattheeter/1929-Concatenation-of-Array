9e2aba89b829eee717854298eb686e55de4ee66a
Matthew Heeter <matthewheeter1611@gmail.com>
2023-04-27 23:46:43 -0400

    Added extend method solution to test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+# Trying out the extend method
+nums = [1,2,1]
+nums.extend(nums)
+print(nums)
+# Appears to do the same as the *2 method
+# Does not have a return value, it modifies the original list
